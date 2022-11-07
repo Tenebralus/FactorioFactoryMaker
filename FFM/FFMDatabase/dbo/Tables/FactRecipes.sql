520671d8e9698d2e4378b3c8432553444bf4d904
Tenebralus <legendariuszz@hotmail.com>
2022-11-07 17:42:36 +0100

    Database added to solution
    Added an SQL Server Database Project and imported the project's database

---
+++ FFM/FFMDatabase/dbo/Tables/FactRecipes.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[FactRecipes] (
+    [RecipeID]     INT      IDENTITY (0, 1) NOT NULL,
+    [CraftingTime] SMALLINT NOT NULL,
+    CONSTRAINT [PK_Recipe] PRIMARY KEY CLUSTERED ([RecipeID] ASC)
+);
+
---
+++ FFM/FFMDatabase/dbo/Tables/FactRecipesMachinesOutputProducts.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[FactRecipesMachinesOutputProducts] (
+    [RecipeID]            INT NOT NULL,
+    [MachineID]           INT NOT NULL,
+    [OutputProductID]     INT NOT NULL,
+    [OutputProductAmount] INT DEFAULT ((0)) NOT NULL,
+    FOREIGN KEY ([MachineID]) REFERENCES [dbo].[FactMachines] ([MachineID]),
+    FOREIGN KEY ([OutputProductID]) REFERENCES [dbo].[FactProducts] ([ProductID]),
+    FOREIGN KEY ([RecipeID]) REFERENCES [dbo].[FactRecipes] ([RecipeID])
+);
+
